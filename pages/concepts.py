78b6f7f9bc166c9ca6dfe9a8722c24ce02350828
Eric Ferrer-Vaughn <eferrervaughn@gmail.com>
2023-10-12 13:16:40 -0700

    Cyber Concepts

---
+++ pages/concepts.py
@@ -0,0 +1,30 @@
+
+import streamlit as st
+import json
+import os
+
+# Load the cybersecurity concepts from the JSON file
+with open(os.path.join(os.path.dirname(__file__), 'InfoSecConcepts.json'), 'r') as file:
+    concepts = json.load(file)
+
+# Extract titles for the autocomplete search box
+titles = [concept["Title"] for concept in concepts]
+
+# Streamlit UI
+st.title("Cybersecurity Concepts Search")
+selected_title = st.selectbox("Search for a concept:", titles, format_func=lambda x: x)
+
+# Display the information for the selected concept
+for concept in concepts:
+    if concept["Title"] == selected_title:
+        st.subheader("Questions")
+        for question in concept["Questions"]:
+            st.write("- " + question)
+
+        st.subheader("Objectives")
+        for objective in concept["Objectives"]:
+            st.write("- " + objective)
+
+        st.subheader("Vocabulary")
+        for word in concept["Vocabulary"]:
+            st.write("- " + word)
